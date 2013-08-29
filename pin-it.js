cbebd5e67204ec628cd1f1fb90551688779438f2
interestudy <adbig37@gmail.com>
2013-08-29 19:33:06 +0800

    Update pin-it.js

--- pin-it.js
+++ pin-it.js
@@ -2,7 +2,7 @@
     function stopvideo(){
          //var flashplayer = document.getElementById('flashPlayer');
          myMovie.Rewind();
-         alert("hello");
+        
     }
     
     stopvideo();
