f9ef3ff141891ad8692201837686f1d22f68131a
interestudy <adbig37@gmail.com>
2013-08-31 20:41:27 +0800

    Update pin-it.js

--- pin-it.js
+++ pin-it.js
@@ -1,4 +1,22 @@
-
+function getFlashMovieObject(movieName){
+    if (window.document[movieName]){
+            
+      return window.document[movieName];
+  }
+  if (navigator.appName.indexOf("Microsoft Internet")==-1){
+ 
+    if (document.embeds && document.embeds[movieName])
+      return document.embeds[movieName];
+  }
+  else // if (navigator.appName.indexOf("Microsoft Internet")!=-1)
+  {
+    return document.getElementById(movieName);
+  }
+}
+        var movie = getFlashMovieObject(myMovie);
+        movie.gotoframe(11);
+        movie.play();
+       
         alert("ok");
    
     
