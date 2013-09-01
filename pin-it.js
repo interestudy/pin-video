9e74f3e735b73a75ad682ee669c32498d7ca70d9
interestudy <adbig37@gmail.com>
2013-09-01 09:22:56 +0800

    Update pin-it.js

--- pin-it.js
+++ pin-it.js
@@ -1,21 +1,22 @@
-if (navigator.appName.indexOf("Microsoft Internet")==-1) {
-    if (document.embeds && document.embeds[id]){
- //   	if(navigator.userAgent.indexOf("Firefox")>0){
- //   		chartRef = document.embeds[id];
- //   	}else{
- //   		chartRef = window[id];
- //   	}
-     alert("firefox");
-    }else{
-	// 	chartRef  = window.document[id];
-	alert("chrome");
-    }
-  alert("others");	     
-  }
-  else {                  
-    //chartRef = window[id];
-    alert("microsoft");
-  }
+// if (navigator.appName.indexOf("Microsoft Internet")==-1) {
+//     if (document.embeds && document.embeds[id]){
+//  //   	if(navigator.userAgent.indexOf("Firefox")>0){
+//  //   		chartRef = document.embeds[id];
+//  //   	}else{
+//  //   		chartRef = window[id];
+//  //   	}
+//      alert("firefox");
+//     }else{
+// 	// 	chartRef  = window.document[id];
+// 	alert("chrome");
+//     }
+//   alert("others");	     
+//   }
+//   else {                  
+//     //chartRef = window[id];
+//     alert("microsoft");
+//   }
+ alert(window.document.getElementById('movie_player').FrameNum);
         
    alert("ok");
    
