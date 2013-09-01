7bd0030fd790b1704f0b707ce68ed3dc2b2436d0
interestudy <adbig37@gmail.com>
2013-09-01 11:50:45 +0800

    Update pin-it.js

--- pin-it.js
+++ pin-it.js
@@ -1,6 +1,8 @@
 alert(window.document['myMovie']);
 alert(document.getElementById('myMovie'));
+alert(document.embeds);
 alert(document.embeds['myMovie']);
+document.embeds['myMovie'].StopPlay();
 alert("ok");
 
 
