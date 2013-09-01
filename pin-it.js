ff50f5d78d0e7424acfeb153398f3cbd63af1489
interestudy <adbig37@gmail.com>
2013-09-01 09:11:21 +0800

    Update pin-it.js

--- pin-it.js
+++ pin-it.js
@@ -1,22 +1,22 @@
-
 if (navigator.appName.indexOf("Microsoft Internet")==-1) {
-    if (document.embeds && document.embeds[id]){
-    	if(navigator.userAgent.indexOf("Firefox")>0){
-    		chartRef = document.embeds[id];
-    	}else{
-    		chartRef = window[id];
-    	}
-    }
-	else{
-		chartRef  = window.document[id];
-	}
-	     
+ //   if (document.embeds && document.embeds[id]){
+ //   	if(navigator.userAgent.indexOf("Firefox")>0){
+ //   		chartRef = document.embeds[id];
+ //   	}else{
+ //   		chartRef = window[id];
+ //   	}
+ //   }
+	// else{
+	// 	chartRef  = window.document[id];
+	// }
+  alert("others");	     
   }
   else {                  
-    chartRef = window[id];
+    //chartRef = window[id];
+    alert("microsoft");
   }
         
-        alert("ok");
+   alert("ok");
    
     
    
