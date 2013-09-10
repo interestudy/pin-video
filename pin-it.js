0219464fd2f0ae8084f0322b00682e63e1168ebf
interestudy <adbig37@gmail.com>
2013-09-10 22:34:51 +0800

    Update pin-it.js

--- pin-it.js
+++ pin-it.js
@@ -1,5 +1,5 @@
-// var html =" <fieldset  style="font-size:30px; z-index:1000000000000000000000;padding-left:0px;padding-right:0px;padding-top: 5px; padding-bottom:5px; color:#FFFFFF; border:none; background:#65a9d7; text-align:center; width:100%;"><span style=" vertical-align:-3px; display:inline-block;">时间:</span><input type="input" style="width:10%; height:50px;"><span style=" vertical-align:-3px; display:inline-block;">评价:</span><input style="width:60%; height:50px; "><div style=" background:#E419AB; border:none; padding:8px 40px; color:white; display:inline-block; vertical-align:-6px;cursor:pointer; margin-left:10px;">分享</div></fieldset>";
-
-// $("body").append(html);
+    var html =  "<fieldset  style='font-size:30px; z-index:100000000000000000; padding-left:0px;padding-right:0px;padding-top: 5px; padding-bottom:5px; color:#005CFF; border:none; background:#65a9d7; text-align:center; width:100%;'><span style=' vertical-align:-3px; display:inline-block;'>时间:</span><input type='input' style='width:10%; height:50px;'><span style=' vertical-align:-3px; display:inline-block;'>评价:</span><input style='width:60%; height:50px; '><div style=' background:#E419AB; border:none; padding:8px 40px; color:white; display:inline-block; vertical-align:-6px;cursor:pointer; margin-left:10px;'>分享</div></fieldset>";
+     
+ $("body").append(html);
 
 alert("odl");
