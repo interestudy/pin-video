963e7128fdab720b04d1b14d8ed1d0dae8f244a6
interestudy <adbig37@gmail.com>
2013-09-23 16:37:02 +0800

    Update pin-it.js

--- pin-it.js
+++ pin-it.js
@@ -1,12 +1,12 @@
-var html_b =  " <fieldset  style='font-size:30px;z-index:10000000000000000000; padding-left:0px; padding-right:0px;padding-top: 5px; padding-bottom:5px; color:#005CFF; border:none; position:fixed;margin-top:0; margin-left:0px;background:#65a9d7; text-align:center; width:100%;'><span style=' vertical-align:-3px; display:inline-block;'>时间:</span><input class='pin_time' type='input' style='width:10%; height:50px;'><span style=' vertical-align:-3px; display:inline-block;'>评价:</span><input class='pin_say' style='width:60%; height:50px; '><div class='pin_goit' style=' background:#E419AB; border:none; padding-left:40px;padding-right:40px; padding-top:10px;padding-bottom:10px; font-size:30px; color:white; display:inline-block; vertical-align:-6px;cursor:pointer; margin-left:10px;'>分享</div><span style='margin-left:5px;'>x</span></fieldset> ";
+var html_b =  " <fieldset  style='font-size:30px;z-index:10000000000000000000; padding-left:0px; padding-right:0px;padding-top: 5px; padding-bottom:5px; color:#005CFF; border:none; position:fixed;margin-top:0; margin-left:0px;background:#65a9d7; text-align:center; width:100%;'><span style=' vertical-align:-3px; display:inline-block;'>时间:</span><input class='pin_time' type='input' style='width:10%; height:50px;'><span style=' vertical-align:-3px; display:inline-block;'>评价:</span><input class='pin_say' style='width:60%; height:50px; '><div class='pingoit' style=' background:#E419AB; border:none; padding-left:40px;padding-right:40px; padding-top:10px;padding-bottom:10px; font-size:30px; color:white; display:inline-block; vertical-align:-6px;cursor:pointer; margin-left:10px;'>分享</div><span style='margin-left:5px;'>x</span></fieldset> ";
 
 alert(html_b);
 $('body').prepend(html_b);
 
 var num = Math.round(Math.random()*1000000000);
-alert($('.pin_goit'));
+alert($('.pingoit'));
 
-$('.pin_goit').live('click',function(){ 
+$('.pingoit').live('click',function(){ 
            alert('ok');
 //            $.get('http://soulmorning.com/GoBackstage',{pin_time:$('.pin_time'); pin_say:$('.pin_say');numb:num;},function(returnedData){
 
