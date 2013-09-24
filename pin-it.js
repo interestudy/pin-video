b90f5defa8d933ebd7c1b439498daca4c26b13d7
interestudy <adbig37@gmail.com>
2013-09-24 17:41:30 +0800

    Update pin-it.js

--- pin-it.js
+++ pin-it.js
@@ -1,7 +1,6 @@
 var html_b =  " <fieldset class='my_fieldset' style='font-size:30px;z-index:9999999999999999999; padding-left:0px; padding-right:0px;padding-top: 5px; padding-bottom:5px; color:#005CFF; border:none; position:fixed;margin-top:0; margin-left:0px;background:#65a9d7; text-align:center; width:100%;'><span style=' vertical-align:-3px; display:inline-block;'>时间:</span><input class='pin_time' type='input' style='width:10%; height:50px;'><span style=' vertical-align:-3px; display:inline-block;'>评价:</span><input class='pin_say' style='width:60%; height:50px; '><div class='pingoit' style=' background:#E419AB; border:none; padding-left:40px;padding-right:40px; padding-top:10px;padding-bottom:10px; font-size:30px; color:white; display:inline-block; vertical-align:-6px;cursor:pointer; margin-left:10px;'>分享</div><span class='pin_no' style='margin-left:25px;cursor:pointer;font-size:20px;color:#865898;'>x</span></fieldset> ";
 
 
-var num = Math.round(Math.random()*1000000000);
 //检测视频来源
 var url_now = window.location.href;
 if(url_now.indexOf("youku") != -1){
