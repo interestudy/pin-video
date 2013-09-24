04fdb08e1db45d3e589c54961273c9abcd68a121
interestudy <adbig37@gmail.com>
2013-09-24 17:33:29 +0800

    Update pin-it.js

--- pin-it.js
+++ pin-it.js
@@ -1,4 +1,4 @@
-var html_b =  " <fieldset  style='font-size:30px;z-index:10000000000000000000; padding-left:0px; padding-right:0px;padding-top: 5px; padding-bottom:5px; color:#005CFF; border:none; position:fixed;margin-top:0; margin-left:0px;background:#65a9d7; text-align:center; width:100%;'><span style=' vertical-align:-3px; display:inline-block;'>时间:</span><input class='pin_time' type='input' style='width:10%; height:50px;'><span style=' vertical-align:-3px; display:inline-block;'>评价:</span><input class='pin_say' style='width:60%; height:50px; '><div class='pingoit' style=' background:#E419AB; border:none; padding-left:40px;padding-right:40px; padding-top:10px;padding-bottom:10px; font-size:30px; color:white; display:inline-block; vertical-align:-6px;cursor:pointer; margin-left:10px;'>分享</div><span style='margin-left:5px;'>x</span></fieldset> ";
+var html_b =  " <fieldset class='my_fieldset' style='font-size:30px;z-index:10000000000000000000; padding-left:0px; padding-right:0px;padding-top: 5px; padding-bottom:5px; color:#005CFF; border:none; position:fixed;margin-top:0; margin-left:0px;background:#65a9d7; text-align:center; width:100%;'><span style=' vertical-align:-3px; display:inline-block;'>时间:</span><input class='pin_time' type='input' style='width:10%; height:50px;'><span style=' vertical-align:-3px; display:inline-block;'>评价:</span><input class='pin_say' style='width:60%; height:50px; '><div class='pingoit' style=' background:#E419AB; border:none; padding-left:40px;padding-right:40px; padding-top:10px;padding-bottom:10px; font-size:30px; color:white; display:inline-block; vertical-align:-6px;cursor:pointer; margin-left:10px;'>分享</div><span class='pin_no' style='margin-left:25px;cursor:pointer;font-size:20px;color:#865898;'>x</span></fieldset> ";
 
 
 var num = Math.round(Math.random()*1000000000);
@@ -7,18 +7,29 @@ var url_now = window.location.href;
 if(url_now.indexOf("youku") != -1){
           
           $('body').prepend(html_b);
-          var flashvars_value = $("param[name='flashvars']").attr('value');
-          var nums = flashvars_value.indexOf('&');
-          var video_id = flashvars_value.substring(0,nums);
-       
-          var my_url = "http://soulmorning.com/GoBackstage"
-                    + "?" + video_id;
 
 }
 
+$(".pin_no").live("click",function(){
+  $(".my_fieldset").remove();
+});
           
 $('.pingoit').live('click',function(){ 
-    alert('ok');
-    window.open(my_url);
+          var flashvars_value = $("param[name='flashvars']").attr('value');
+          var nums = flashvars_value.indexOf('&');
+          var video_id = flashvars_value.substring(0,nums);
+          var video_time = $(".pin_time").val();
+          if($.trim(video_time).length <= 0 ) alert("输入你分享的那么时间点");
+          var video_comment = $(".pin_say").val();
+          if($.trim(video_comment).length <= 0 ) alert("关于这个镜头，你想说些什么？");
+          
+          var my_url = "http://soulmorning.com/GoBackstage"
+                    + "?" + video_id
+                    + "&video_time=" + video_time
+                    + "&video_comment=" + video_comment;
+          if($.trim(video_time).length >= 0 && $.trim(video_comment).length >= 0 ){
+            window.open(my_url);
+          }
+          
  });
 
