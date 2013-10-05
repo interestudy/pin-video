5cefe0d52933c65a218b01c5e5a408a68877bb41
interestudy <adbig37@gmail.com>
2013-10-05 21:53:05 +0800

    Update pin-it.js

--- pin-it.js
+++ pin-it.js
@@ -2,7 +2,7 @@ var html_b =  " <fieldset class='my_fieldset' style='font-size:30px;z-index:9999
 // alert($("div##vpvideotitlev5_wrap .title").html());
 // alert($("div#vpvideotitlev5 h1").html());
 // alert($("#vpvideotitlev5_wrap h1").children('a').length  +　$("#vpvideotitlev5_wrap h1 a").html()); 
-    var _h1 =  $("##vpvideotitlev5_wrap h1").html();alert(_h1 + "_h1");
+    var _h1 =  $("#vpvideotitlev5_wrap h1").html();alert(_h1 + "_h1");
 //检测视频来源
 var url_now = window.location.href; 
 if(url_now.indexOf("youku") != -1 && url_now.indexOf(".html") != -1){
@@ -36,7 +36,7 @@ $('.pingoit').live('click',function(){
           var _h1_a =  $("#vpvideotitlev5_wrap h1 a").length;alert(_h1_a + "_h1_a");
           var _h1_children = $("#vpvideotitlev5_wrap h1").children().length;alert( _h1_children + "_h1");
           var _h1_span = $("#vpvideotitlev5_wrap h1").children("span").length;alert(_h1_span + "_h1_span");
-           var _h1 =  $("##vpvideotitlev5_wrap h1").length;alert(_h1 + "_h1");
+           var _h1 =  $("#vpvideotitlev5_wrap h1").length;alert(_h1 + "_h1");
            
           if(_h1_children > 0){
                   if(_h1_span > 0 && _h1_a > 0){
@@ -50,7 +50,7 @@ $('.pingoit').live('click',function(){
                   }
           }else 
           if( _h1_children == 0){alert('adad');
-                    video_title = $("##vpvideotitlev5_wrap h1").html();
+                    video_title = $("#vpvideotitlev5_wrap h1").html();
           }
 
           alert(video_title);
